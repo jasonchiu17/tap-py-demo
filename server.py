696afd9ecba1878cf806acf0aa82a542c61d95d9
Jason Chiu <jason.chiu172@gmail.com>
2022-12-08 11:16:34 +0800

    demo code for zerone

--- server.py
+++ server.py
@@ -1,11 +1,25 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return render_template('index.html')
+
+@app.route('/api')
+def api():
+    resp = { 
+    'data' : { 
+        'd1': 'hello world',
+        'd2': 'test'
+    } ,
+    'info' : { 
+        'app_name' : 'TAP python demo',
+        'data_from' : 'ZERONE'
+        }
+    }
+    return jsonify(resp)
 
 if __name__ == '__main__':
     app.run()
